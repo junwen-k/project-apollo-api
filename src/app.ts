e2fb150935b446471c2d7c71484e55ef0906652a
junwen-k <junwenkwan97@gmail.com>
2022-01-23 00:17:09 +0800

    feat: implement ml for priority

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { v4 as uuidv4 } from 'uuid';
 
 import { PrismaClient } from '@prisma/client';
+import * as toxicity from '@tensorflow-models/toxicity';
 import * as use from '@tensorflow-models/universal-sentence-encoder';
 import translate from '@vitalets/google-translate-api';
 
@@ -11,9 +12,56 @@ import loadAndPredict from './model';
 
 const prisma = new PrismaClient();
 
-const { MODEL_FEEDBACK_CATEGORY_CLASSIFIER_URL, PORT = 3000 } = process.env;
+const {
+  CONFIDENCE_THRESHOLD = 0.9,
+  MODEL_FEEDBACK_CATEGORY_CLASSIFIER_URL,
+  PORT = 3000,
+} = process.env;
 
-const CATEGORY_MAP = ['insightful', 'non-insightful', 'low-quality'];
+const CATEGORY_MAP = ['insightful', 'non-insightful', 'low-quality'] as const;
+
+const predictFeedbackCategory = async (text: string) => {
+  const dataArray = await loadAndPredict(
+    MODEL_FEEDBACK_CATEGORY_CLASSIFIER_URL,
+    await (await use.load()).embed(text)
+  );
+
+  let category: typeof CATEGORY_MAP[keyof typeof CATEGORY_MAP] = 'low-quality';
+  if (dataArray) {
+    // Find the index of the highest confidence score.
+    const idx = Array.from(dataArray).indexOf(Math.max(...dataArray));
+    category = CATEGORY_MAP[idx];
+  }
+
+  return category;
+};
+
+const predictPriority = async (
+  text: string,
+  category: typeof CATEGORY_MAP[keyof typeof CATEGORY_MAP],
+  rating: number
+) => {
+  const model = await toxicity.load(Number(CONFIDENCE_THRESHOLD), ['toxicity']);
+
+  const predictions = await model.classify(text);
+
+  const isToxic = predictions.some((prediction) =>
+    prediction.results.some((result) => result.match)
+  );
+
+  let priority = 0;
+  if (isToxic) {
+    priority++;
+  }
+  if (rating < 5) {
+    priority++;
+  }
+  if (category === 'insightful') {
+    priority++;
+  }
+
+  return priority;
+};
 
 const app = express();
 
@@ -24,23 +72,17 @@ app.get('/', (_, res) => {
 });
 
 app.post('/feedbacks', async (req, res) => {
-  const { name, email, contactNo, product, rating, verbatim } = req.body;
+  const { name, email, contactNo, product, rating = 0, verbatim } = req.body;
 
   const translationResult = await translate(verbatim);
 
-  const dataArray = await loadAndPredict(
-    MODEL_FEEDBACK_CATEGORY_CLASSIFIER_URL,
-    await (await use.load()).embed(translationResult.text) // Use translated text to support other languages...
+  const category = await predictFeedbackCategory(translationResult.text); // Use translated text to support other languages...
+
+  const priority = await predictPriority(
+    translationResult.text,
+    category,
+    rating
   );
-
-  let category = 'low-quality';
-  if (dataArray) {
-    // Find the index of the highest confidence score.
-    const idx = Array.from(dataArray).indexOf(Math.max(...dataArray));
-    category = CATEGORY_MAP[idx];
-  }
-
-  // TODO: implement priority ML
 
   const feedback = await prisma.feedback.create({
     data: {
@@ -48,6 +90,7 @@ app.post('/feedbacks', async (req, res) => {
       name,
       email,
       contactNo,
+      priority,
       product,
       rating,
       verbatim,
@@ -58,21 +101,32 @@ app.post('/feedbacks', async (req, res) => {
 });
 
 app.get('/feedbacks', async (req, res) => {
-  const { limit, offset, keyword, rating, product, category } = req.query;
+  const { limit, offset, keyword, rating, product, priority, category } =
+    req.query;
   const feedbacks = await prisma.feedback.findMany({
     where: {
       category: category?.toString(),
       email: { contains: keyword?.toString() },
       name: { contains: keyword?.toString() },
       product: product?.toString(),
+      priority: priority ? Number(priority) : undefined,
       rating: rating ? Number(rating) : undefined,
     },
+    orderBy: { createdAt: 'desc' },
     take: limit ? Number(limit) : undefined,
     skip: offset ? Number(offset) : undefined,
   });
+
   const count = await prisma.feedback.count();
   res.setHeader('X-Total-Count', count);
-  res.json(feedbacks);
+
+  // Sorting hack after fetched from database.
+  const sortedFeedbacks = feedbacks.sort(
+    (f1, f2) =>
+      CATEGORY_MAP.indexOf(f1.category as any) -
+      CATEGORY_MAP.indexOf(f2.category as any)
+  );
+  res.json(sortedFeedbacks);
 });
 
 app.get('/feedbacks/:feedbackId', async (req, res) => {
